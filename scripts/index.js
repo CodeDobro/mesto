165bbe2aaf1098acd894bcc8727601593a294dab
Dobro <johndoeelec@yandex.ru>
2021-11-02 16:46:55 +0300

    feat: создать попап

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+const popupElement = document.querySelector('.popup');
+
+const editButton = document.querySelector('.element__edit-button');
+
+const closeButton = popupElement.querySelector('.popup__close-button');
