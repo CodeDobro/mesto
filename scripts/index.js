b2866db4d829c5c8c5c0c91271bc25470665b76d
Dobro <johndoeelec@yandex.ru>
2021-11-26 09:41:38 +0300

    feet: Добавление карточек. Добавлены лайки.

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
 const popupProfile = document.querySelector('.popup__profile');
-const popupAdd = document.querySelector('.popup__add');
 const popupFormProfile = popupProfile.querySelector('.popup__form-profile');
+const popupAdd = document.querySelector('.popup__add');
+const popupFormAdd = popupAdd.querySelector('.popup__form-add');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButtonProfile = popupProfile.querySelector('.popup__close-button');
 const closeButtonAdd = popupAdd.querySelector('.popup__close-button');
 const inputName = popupProfile.querySelector('[name=form-name]');
 const inputAbout = popupProfile.querySelector('[name=form-about]');
+const inputTitle = popupAdd.querySelector('[name=form-title]');
+const inputLink = popupAdd.querySelector('[name=form-link]');
 const title = document.querySelector('.profile__title');
 const subtitle = document.querySelector('.profile__subtitle');
 const saveButton = popupProfile.querySelector('.popup__save-button');
@@ -51,8 +54,6 @@ function closePopup() {
 }
 
 function openPopupAdd() {
-  // inputName.value = title.textContent;
-  // inputAbout.value = subtitle.textContent;
   popupAdd.classList.add('popup_open');
 }
 
@@ -65,7 +66,7 @@ closeButtonProfile.addEventListener('click', closePopup)
 addButton.addEventListener('click', openPopupAdd)
 closeButtonAdd.addEventListener('click', closePopupAdd)
 
-function formSubmitHandler (evt) {
+const formSubmitHandler = (evt) => {
     evt.preventDefault();
 
     title.textContent = inputName.value;;
@@ -73,13 +74,30 @@ function formSubmitHandler (evt) {
     closePopup();
 }
 
+const submitFormAdd = (evt) => {
+  evt.preventDefault();
+
+  const inputAddTitle = inputTitle.value;
+  const inputAddLink = inputLink.value;
+  const elementName = createElementDomNode({ name: inputAddTitle, link: inputAddLink });
+
+  list.prepend(elementName);
+
+  closePopupAdd();
+};
+
 popupFormProfile.addEventListener('submit', formSubmitHandler);
+popupFormAdd.addEventListener('submit', submitFormAdd);
 
 const createElementDomNode = (item) => {
   const elementTemplate = template.content.querySelector('.element').cloneNode(true);
   elementTemplate.querySelector('.element__title').textContent = item.name;
   elementTemplate.querySelector('.element__image').src = item.link;
   elementTemplate.querySelector('.element__image').alt = item.name;
+
+  elementTemplate.querySelector('.element__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_status_active');
+  });
 
   const trashButton = elementTemplate.querySelector('.element__trash');
   trashButton.addEventListener('click', () => {
