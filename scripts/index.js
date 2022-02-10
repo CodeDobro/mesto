77f344269d7128b6ac250a180e1e452cbe5b2d27
Dobro <johndoeelec@yandex.ru>
2022-02-10 17:56:53 +0300

    Исправление ошибок 2.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+import initialCards from "./initialcards.js"
 const popupProfile = document.querySelector('.popup_type_profile');
 const popupFormProfile = popupProfile.querySelector('.popup__form-profile');
 const popupOpenImage = document.querySelector('.popup_type_image');
@@ -19,33 +20,6 @@ const saveButton = popupProfile.querySelector('.popup__save-button');
 const template = document.querySelector('.element__template')
 const addButton = document.querySelector('.profile__add-button');
 const list = document.querySelector('.elements__list');
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 function keyHandler(evt) {
   if (evt.key === 'Escape') {
